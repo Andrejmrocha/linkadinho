2fba67aa1ea19b956c5fb9fbeba77e4f750ce8ab
= <ajonasmorais@gmail.com>
2024-08-31 10:23:57 -0300

    Projeto inicial SpringBoot

---
+++ api-linkadinho/src/main/java/com/linkadinho/api_linkadinho/ApiLinkadinhoApplication.java
@@ -0,0 +1,13 @@
+package com.linkadinho.api_linkadinho;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiLinkadinhoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiLinkadinhoApplication.class, args);
+	}
+
+}
---
+++ api-linkadinho/src/test/java/com/linkadinho/api_linkadinho/ApiLinkadinhoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.linkadinho.api_linkadinho;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiLinkadinhoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
